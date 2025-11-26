0f69b881a56225d4c9201f95c190ca67cb16c4f2
Sachin Jadhav <sachinjdv7@gmail.com>
2025-11-26 16:36:00 +0530

    feat: implement visitor check-out functionality with error handling

--- backend/src/controllers/visitor.controller.js
+++ backend/src/controllers/visitor.controller.js
@@ -1,3 +1,7 @@
+import { Visitor } from "../models/visitor.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
 const visitorIn = async (req, res) => {
   try {
     const {
@@ -74,5 +78,48 @@ const visitorIn = async (req, res) => {
   }
 };
 
+const visitorOut = async (req, res) => {
+  try {
+    if (!req.user) {
+      throw new ApiError(401, "Unauthorized request");
+    }
 
-export { visitorIn };
+    const { visitorId } = req.params;
+
+    const visitor = await Visitor.findById(visitorId);
+
+    if (!visitor) {
+      throw new ApiError(404, "User not found");
+    }
+
+    if (visitor.outTime) {
+      throw new ApiError(400, "Visitor already checked out");
+    }
+
+    const outTime = new Date();
+    const totalTimeSpent = Math.round((outTime - visitor.inTime) / 60000);
+
+    visitor.outTime = outTime;
+    visitor.totalTimeSpent = totalTimeSpent;
+    visitor.updatedBy = req.user.id;
+
+    await visitor.save();
+
+    res
+      .status(200)
+      .json(new ApiResponse(200, visitor, "Visitor checked out successfully"));
+  } catch (error) {
+    console.error("Error while tracking visitor out:", error);
+    res
+      .status(error?.statusCode || 500)
+      .json(
+        new ApiResponse(
+          error?.statusCode || 500,
+          null,
+          error?.message || "Server error"
+        )
+      );
+  }
+};
+
+export { visitorIn, visitorOut };
--- backend/src/routes/visitor.router.js
+++ backend/src/routes/visitor.router.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { visitorIn } from "../controllers/visitor.controller.js";
+import { visitorIn, visitorOut } from "../controllers/visitor.controller.js";
 
 const visitorRouter = express.Router();
 
 visitorRouter.post("/check-in ", visitorIn);
+visitorRouter.patch("/check-out ", visitorOut);
 
 export { visitorRouter };
