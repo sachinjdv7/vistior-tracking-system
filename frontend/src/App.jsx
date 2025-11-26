b6b0484fcf0900a2cd318becfe9ceefccd221827
Sachin Jadhav <sachinjdv7@gmail.com>
2025-11-27 00:59:01 +0530

    feat: implement login component and update app structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Login from "./components/Login";
 
 const App = () => {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-100 via-indigo-100 to-purple-200 px-4">
+      <h1 className="text-3xl md:text-4xl font-bold mb-8 text-gray-800">
+        Login
+      </h1>
+
+      <Login />
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,23 @@
+const Login = () => {
+  return (
+    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full max-w-sm border p-6 shadow-lg">
+      <label className="label">Email</label>
+      <input
+        type="email"
+        className="input input-bordered w-full"
+        placeholder="Email"
+      />
+
+      <label className="label mt-2">Password</label>
+      <input
+        type="password"
+        className="input input-bordered w-full"
+        placeholder="Password"
+      />
+
+      <button className="btn btn-neutral mt-6 w-full">Login</button>
+    </fieldset>
+  );
+};
+
+export default Login;
