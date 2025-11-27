2b66f731b569fe528782578844422cf89f7bf1c4
Sachin Jadhav <sachinjdv7@gmail.com>
2025-11-27 17:24:54 +0530

    fix: cors error frontend and backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { authRouter } from "./routes/auth.router.js";
 import { userRouter } from "./routes/user.router.js";
 import { visitorRouter } from "./routes/visitor.router.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(
   express.json({
     limit: "16kb",
@@ -17,7 +24,6 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/visitor", visitorRouter);
-
 
 app.use("/", (req, res) => {
   res.send("Welcome to visitor tracking system!");
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,15 +1,41 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // console.log(username, password);
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:8082/api/v1/auth/login",
+        {
+          username,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      console.log("Login successful:", res.data);
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
+
   return (
     <>
       <h1 className="text-3xl md:text-4xl font-bold mb-8 text-gray-800">
         Login
       </h1>
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full max-w-sm border p-6 shadow-lg">
-        <label className="label">Email</label>
+        <label className="label">Username</label>
         <input
-          type="email"
+          type="username"
           className="input input-bordered w-full"
-          placeholder="Email"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
         />
 
         <label className="label mt-2">Password</label>
@@ -17,9 +43,12 @@ const Login = () => {
           type="password"
           className="input input-bordered w-full"
           placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button className="btn btn-neutral mt-6 w-full">Login</button>
+        <button onClick={handleLogin} className="btn btn-neutral mt-6 w-full">
+          Login
+        </button>
       </fieldset>
     </>
   );
