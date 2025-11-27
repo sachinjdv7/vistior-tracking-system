31b20acf2a479f918a7f3b84cbd5ce553942d2c7
Sachin Jadhav <sachinjdv7@gmail.com>
2025-11-27 16:50:48 +0530

    feat: added basic navbar and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-100 via-indigo-100 to-purple-200 px-4">
+      <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
